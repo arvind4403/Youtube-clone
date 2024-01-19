89cd000e88d4cd684a50562b27918abcfe7a4312
arvind4403 <kr.arvind027@gmail.com>
2024-01-19 19:36:53 +0530

    HomePage

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import MediaPlayer from '@/app/components/MediaPlayer'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <MediaPlayer/>
+      </div>
+    </main>
+  )
+}
