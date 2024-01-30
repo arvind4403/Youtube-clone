3215d2674f8de7dc3d7a717135f7b9d6bdf2f5fe
arvind4403 <kr.arvind027@gmail.com>
2024-01-30 19:11:23 +0530

    added suggest video and comment sectionv

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 "use client"
-
-import MediaPlayer from '@/app/components/MediaPlayer'
+import React from 'react';
+import Suggestion from './components/Suggestion';
+import MediaPlayer from './components/MediaPlayer';
+// import Suggestion from './components/Suggestion';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <MediaPlayer/>
-      </div>
-    </main>
-  )
+    <div className="bg-gray-900 text-white min-h-screen">
+      <header className="p-4 text-center">
+        <h1 className="text-3xl font-bold">Stream Video</h1>
+      </header>
+      <main className="flex justify-center items-center p-8">
+        <div className="flex">
+          <div className="mr-8">
+            <MediaPlayer />
+          </div>
+          <div className="flex-grow">
+            <Suggestion />
+          </div>
+        </div>
+      </main>
+      <footer className="text-center py-4 bg-gray-800">
+        <p>&copy; 2024 Stream Video. All rights reserved.</p>
+      </footer>
+    </div>
+  );
 }
